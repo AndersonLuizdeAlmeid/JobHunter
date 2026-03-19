d1d040a3f2d87fc1b9b8b90f842a552a0c5dbeb8
Anderson Luiz de Almeida <anderson-luiza@hotmail.com>
2026-03-18 21:25:11 -0300

    First commit for save the first step of this project

---
+++ JobHunter.Domain/Entities/JobOpportunity.cs
@@ -0,0 +1,17 @@
+﻿using JobHunter.Domain.Enums;
+
+namespace JobHunter.Domain.Entities;
+
+public class JobOpportunity
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string ExternalId { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Company { get; set; } = string.Empty;
+    public string? SalaryRange { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool MentionedBlueCard { get; set; } = false;
+    public JobStatus Status { get; set; } = JobStatus.New;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ JobHunter.Domain/Enums/JobStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace JobHunter.Domain.Enums;
+public enum JobStatus
+{
+    New,
+    Applied,
+    Interviewing,
+    Rejected,
+    Ignored
+}
